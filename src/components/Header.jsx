312dd3d442d68c40f1a3eeb080ecfda6e4922ea4
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2022-06-27 14:06:22 -0300

    deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
 import { Logo } from "./Logo";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './header.css'
 
 export function Header() {
   return (
     <header className='flex items-center justify-between bg-blue-500 w-full h-[80px] px-52 text-white'>
-      <div>
+      <NavLink to='/'>
         <Logo />
-      </div>
+      </NavLink>
 
-      <li className='list-none flex gap-x-8'>
-        <Link to="/">HOME</Link>
-        <Link to="/sobre">SOBRE</Link>
-        <Link to="/atendimento">ATENDIMENTO</Link>
-        <Link to="/contato">LOCALIZAÇÃO</Link>
+      <li className='list-none flex gap-x-8 font-bold'>
+        <NavLink to="/" activeClassName="active">HOME</NavLink>
+        <NavLink to="/sobre" activeClassName="active">SOBRE</NavLink>
+        <NavLink to="/atendimento" activeClassName="active">ATENDIMENTO</NavLink>
+        <NavLink to="/contato" activeClassName="active">LOCALIZAÇÃO</NavLink>
       </li>
     </header>
   )
